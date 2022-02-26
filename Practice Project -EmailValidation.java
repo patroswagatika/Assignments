bfc7940a595b78c2a6693927d1ccc06acb9baa77
swagatika patro <swagatikapatro7@gmail.com>
2022-02-27 01:02:02 +0530

    files added

--- Practice Project -EmailValidation.java
+++ Practice Project -EmailValidation.java
@@ -1,5 +1,6 @@
 package EmailvalidPack;
 
+
 import java.util.regex.Pattern;
 
 public class EmailValidation {
@@ -7,9 +8,11 @@ public class EmailValidation {
 	public static void main(String[] args) {
 		String email1 = "simplilearn14@gmail.com";
 		String email2 = "simplilearn.com";
+		String email3 = "com.simplilearn";
 		
 		System.out.println("email1 = " + validateEmail(email1));
 		System.out.println("email2 = " + validateEmail(email2));
+		System.out.println("email3 = " + validateEmail(email3));
 	}
 	
 	public static String validateEmail (String email) {
@@ -28,4 +31,5 @@ public class EmailValidation {
 			return "invalid";
 		}		
 	}
+
 }
